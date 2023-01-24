198ac01463e601da7d05527e374b667a36d8e9ef
smeetChheda <contact.smeet@gmail.com>
2023-01-24 15:12:52 -0500

    inital commit - took out next.js extras, integrated tailwind, made boilerplate code for different pages

---
+++ pages/events/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default function EventsPage() {
+    return (
+        <>
+            <Head>
+                <title>Events</title>
+                <meta name="description" content="Queen's Asian Student Association" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <div>
+                    Events
+                </div>
+            </main>
+        </>
+    )
+}
---
+++ pages/execs/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default function ExecsPage() {
+    return (
+        <>
+            <Head>
+                <title>Club Executives</title>
+                <meta name="description" content="Queen's Asian Student Association" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <div>
+                    Execs
+                </div>
+            </main>
+        </>
+    )
+}
