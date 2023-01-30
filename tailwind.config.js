b129cc67dc1d1d93fcf652d103f824bf0c1a342c
smeetChheda <contact.smeet@gmail.com>
2023-01-30 15:13:53 -0500

    updated tailwind config to include qasa theme, screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '320px',
+        'sm': '425px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1440px',
+      }
+    },
+    colors: {
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'red-dark': '#E03F23',
+      'red-light': '#E75E4B',
+      'beige': '#FDF5EA',
+      'blue-light': '#99BBCC',
+      'blue-dark': '#7698B3'
+    }
   },
   plugins: [],
+  darkMode: 'class',
 }
