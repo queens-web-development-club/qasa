84fd38260af26abb8778a31bde15fa3451ded1a6
smeetChheda <contact.smeet@gmail.com>
2023-02-01 21:38:00 -0500

    whole site formatting stuff

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>
-          home
+      <main className="bg-white">
+        <div className="h-screen mx-48">
         </div>
       </main>
     </>
