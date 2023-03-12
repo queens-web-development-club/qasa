3aa3f75e3d393dc56855c944d976c2085b1b5c2f
sirivanbiscuit <sirivanbiscuit@gmail.com>
2023-03-12 16:33:22 -0400

    initial footer commit

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return(
+        <footer className="bg-beige h-[328.13px]" >
+            <div className="text-black 
+            left-[3.3%] right-[56.01%] top-[5.33%] bottom-[63.06%]
+            text-['] font-normal text-[80px]">
+                Stay connected
+            </div>
+        </footer>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import Footer from '@/components/Footer'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Footer />
     </>
   ) 
   
