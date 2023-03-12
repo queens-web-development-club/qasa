4dbd7beecb6a8debe9cbdc4299664f1755991173
smeetChheda <contact.smeet@gmail.com>
2023-03-12 19:30:54 -0400

    fixed background colors

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <div className="bg-white">
       <Component {...pageProps} />
-    </>
+    </div>
   ) 
   
 }
