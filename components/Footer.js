7e9d6a6b61e53cfbc5e7fe24039a1fc53b19fc13
sirivanbiscuit <sirivanbiscuit@gmail.com>
2023-03-14 16:07:52 -0400

    Added social links

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,18 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import FbLogo from "../assets/images/logos/fb.png";
 import InstaLogo from "../assets/images/logos/insta.png";
 import TreeLogo from "../assets/images/logos/tree.png";
 import MailSignUp from './MailSignUp';
 
-
 export default function Footer() {
     return(
-        <footer className="bg-beige px-8 pb-24">
+        <footer className="bg-beige px-8 pb-16 pt-4">
             {/** Footer Prompt Container */}
             <div>
                 {/** Title */}
                 <div className="text-black 
-                text-[40px] md:text-[60px] lg:text-[80px]">
+                text-[40px] md:text-[60px] lg:text-[70px]">
                     Stay connected
                 </div>
                 {/** Subtext */}
@@ -20,12 +20,33 @@ export default function Footer() {
                 text-[20px] md:text:[30px] lg:text-[40px]">
                     Sign up for our mailing list.
                 </div>
-                {/** Links */}
+
+                {/** Socials */}
                 <div className='flex flex-row flex-wrap lg:flex-nowrap'>
+                {/** Mail Sign Up Box */}
                 <MailSignUp />
-                <Image src={FbLogo} className="ml-auto mr-[2%] h-20 w-20"></Image>
-                <Image src={InstaLogo} className="mr-[2%] h-20 w-20"></Image>
-                <Image src={TreeLogo} className="h-20 w-20"></Image>
+                {/** Facebook */}
+                <Link href="https://www.facebook.com/queens.qasa"
+                className="ml-auto mr-[2%] h-20 w-20 
+                hover:shadow-xl duration-300 rounded-[22px]"
+                target="_blank">
+                    <Image src={FbLogo} />
+                </Link>
+                {/** Instagram */}
+                <Link href="https://www.instagram.com/queens.asa/"
+                className=" mr-[2%] h-20 w-20 
+                hover:shadow-xl duration-300 rounded-[22px]"
+                target="_blank">
+                    <Image src={InstaLogo} />
+                </Link>
+                {/** Linktree */}
+                <Link href="https://linktr.ee/qasa"
+                className="mr-[2%] h-20 w-20 
+                hover:shadow-xl duration-300 rounded-[22px]"
+                target="_blank">
+                    <Image src={TreeLogo} />
+                </Link>
+
                 </div>
             </div>
         </footer>
--- components/MailSignUp.js
+++ components/MailSignUp.js
@@ -1,11 +1,13 @@
 export default function MailSignUp() {
     return(
-        <div className="flex w-max py-4 lg:w-[70%] lg:py-0">
+        <div className="flex w-[100%] py-4 lg:w-[70%] lg:py-0 lg:pr-[5%]">
             <input type="text" placeholder="Email" name="email" 
-            className="h-full w-full bg-white text-black text-[15px] md:text:[25px] lg:text-[35px] pl-4 rounded-l-[10px]">
+            className="h-10 md:h-20 w-full bg-white text-black 
+            text-[20px] md:text:[30px] lg:text-[35px] 
+            pl-4 rounded-l-[10px]">
             </input>
             <button className="bg-blue-dark text-white
-            text-[15px] md:text:[25px] lg:text-[35px] px-[5%]
+            text-[20px] md:text:[30px] lg:text-[35px] px-[5%]
             hover:shadow-xl duration-300 rounded-r-[10px]">
                 Subscribe
             </button>
