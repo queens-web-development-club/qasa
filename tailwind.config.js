9340749d3860acb8457b9c5daea373f9221c4eca
smeetChheda <contact.smeet@gmail.com>
2023-03-15 16:16:17 -0400

    fixed fadeout definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,8 @@ module.exports = {
           "100%": { opacity: 1 }
         },
         fadeOut: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 }
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 }
         }
       }
     },
