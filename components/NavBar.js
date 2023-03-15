13f7166eb3f636ffc7e8c3191d2fa854f789c276
smeetChheda <contact.smeet@gmail.com>
2023-03-15 16:14:06 -0400

    cleaned up styling

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,12 +5,11 @@ import { useState, useEffect, useCallback } from 'react';
 import NavLogo from "@/assets/images/qasa_nav_logo.png";
 
 export default function NavBar() {
-    const [nav, setNav] = useState(false);
+    // Enable when implementing mobile support
+    // const [nav, setNav] = useState(false); 
     const [logo, setLogo] = useState(false);
 
-    const toggleLogo = useCallback(event => {
-        
-
+    const toggleLogo = useCallback(() => {
         if (window.scrollY > 10) setLogo(true)
         else setLogo(false)
     }, []);
@@ -21,47 +20,44 @@ export default function NavBar() {
     }, []);
 
     return (
-        <header className="fixed top-0 left-0 w-full z-30">
-            <div className="relative">
-                <nav className="w-full bg-beige">
-                    <div className="flex flex-row justify-center gap-[50px] px-10 py-5">
-                        <div className={`flex gap-12 align-center transition-all duration-500 ${logo ? "-translate-x-40" : ""}`}>
-                            <Link href="/">
-                                <div className="nav-item">
-                                    Home
-                                </div>
-                            </Link>
-                            <Link href="/about">
-                                <div className="nav-item">
-                                    About
-                                </div>
-                            </Link>
-                        </div>
-                        
-                        {logo && 
+        <header className="fixed top-0 left-0 w-full z-30 bg-beige">
+            <nav>
+                <div className="flex flex-row justify-center gap-[50px] px-10 py-2">
+                    <div className={`flex gap-12 align-center transition-all duration-500 ${logo ? "-translate-x-40" : ""}`}>
                         <Link href="/">
-                            <div className={`${logo ? "animate-fadeIn" : "animate-fadeOut"} transition-opacity duration-500`}>
-                                <Image src={NavLogo} alt="" width={50} height={50}/> 
+                            <div className="nav-item">
+                                Home
                             </div>
                         </Link>
-                        }
-                        <div className={`flex gap-12 align-center transition-all duration-500 ${logo ? "translate-x-40" : ""}`}>
-                            <Link href="/events">
-                               <div className="nav-item">
-                                    Events
-                                </div> 
-                            </Link>
-                            
-                            <Link href="/">
-                                <div className="nav-item">
-                                    Contact
-                                </div>
-                            </Link>
-                            
-                        </div>
+                        <Link href="/about">
+                            <div className="nav-item">
+                                About
+                            </div>
+                        </Link>
                     </div>
-                </nav>
-            </div>
+                    
+                    {logo && 
+                    <Link href="/">
+                        <div className="animate-fadeIn transition-opacity duration-500">
+                            <Image className="p-5" src={NavLogo} alt="" fill objectFit='contain'/> 
+                        </div>
+                    </Link>
+                    }
+                    <div className={`flex gap-12 align-center transition-all duration-500 ${logo ? "translate-x-40" : ""}`}>
+                        <Link href="/events">
+                            <div className="nav-item">
+                                Events
+                            </div> 
+                        </Link>
+                        
+                        <Link href="/">
+                            <div className="nav-item">
+                                Contact
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
             
         </header>
     )
