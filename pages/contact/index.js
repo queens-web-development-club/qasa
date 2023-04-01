f20bedca7150bcefcc309865a84cce4778e7a91f
smeetChheda <contact.smeet@gmail.com>
2023-04-01 14:43:26 -0400

    finished contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
-import ImageCarousel from '@/components/ImageCarousel';
 import FbLogo from "@/assets/images/logos/fb.png";
 import InstaLogo from "@/assets/images/logos/insta.png";
 import TreeLogo from "@/assets/images/logos/tree.png";
+
+import SeniorLeadershipPhoto from '@/assets/images/team-photos/senior-leadership.png';
+import MarketingPhoto from '@/assets/images/team-photos/marketing.png';
+import OutreachPhoto from '@/assets/images/team-photos/outreach.png';
+import SponsorshipsPhoto from '@/assets/images/team-photos/sponsorships.png';
+import LogisticsPhoto from '@/assets/images/team-photos/logistics.png';
+
 
 import exImage from "@/assets/images/image-carousel/carousel-img-1.png"
 const carouselImages = [exImage, exImage, exImage, exImage, exImage, exImage]
@@ -52,25 +58,25 @@ export default function ExecsPage() {
                         Meet the Team
                     </div>
                     <div className="flex flex-wrap">
-                        <Link href="/contact/senior-leadership" className="w-full lg:w-1/2 lg:px-2 cursor-pointer">
-                            <ImageCarousel images={carouselImages} />
-                            <div className="flex justify-center text-xl"> Senior Leadership </div>
+                        <Link href="/contact/senior-leadership" className="w-full lg:w-1/2 lg:px-2 cursor-pointer py-8">
+                            <Image src={SeniorLeadershipPhoto} className="mx-auto"/>
+                            <div className="flex justify-center pt-4 text-2xl md:text-4xl"> Senior Leadership </div>
                         </Link>
-                        <Link href="/contact/marketing" className="w-full lg:w-1/2 lg:px-2 cursor-pointer">
-                            <ImageCarousel images={carouselImages} />
-                            <div className="flex justify-center text-xl"> Marketing </div>
+                        <Link href="/contact/marketing" className="w-full lg:w-1/2 lg:px-2 cursor-pointer py-8">
+                            <Image src={MarketingPhoto} className="mx-auto"/>
+                            <div className="flex justify-center pt-4 text-2xl md:text-4xl"> Marketing </div>
                         </Link>
-                        <Link href="/contact/outreach" className="w-full lg:w-1/2 lg:px-2 cursor-pointer">
-                            <ImageCarousel images={carouselImages} />
-                            <div className="flex justify-center text-xl"> Outreach </div>
+                        <Link href="/contact/outreach" className="w-full lg:w-1/2 lg:px-2 cursor-pointer py-8">
+                            <Image src={OutreachPhoto} className="mx-auto"/>
+                            <div className="flex justify-center pt-4 text-2xl md:text-4xl"> Outreach </div>
                         </Link>
-                        <Link href="/contact/sponsorships" className="w-full lg:w-1/2 lg:px-2 cursor-pointer">
-                            <ImageCarousel images={carouselImages} />
-                            <div className="flex justify-center text-xl"> Sponsorships </div>
+                        <Link href="/contact/sponsorships" className="w-full lg:w-1/2 lg:px-2 cursor-pointer py-8">
+                            <Image src={SponsorshipsPhoto} className="mx-auto"/>
+                            <div className="flex justify-center pt-4 text-2xl md:text-4xl"> Sponsorships </div>
                         </Link>
-                        <Link href="/contact/logistics" className="w-full lg:w-1/2 lg:px-2 cursor-pointer">
-                            <ImageCarousel images={carouselImages} />
-                            <div className="flex justify-center text-xl"> Logistics </div>
+                        <Link href="/contact/logistics" className="w-full lg:w-1/2 lg:px-2 cursor-pointer py-8">
+                            <Image src={LogisticsPhoto} className="mx-auto"/>
+                            <div className="flex justify-center pt-4 text-2xl md:text-4xl"> Logistics </div>
                         </Link>
                     </div>
                 </div>
