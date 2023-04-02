8b35963c74ccd861b1087c0ca2f2586414196988
smeetChheda <contact.smeet@gmail.com>
2023-04-01 22:59:59 -0400

    events page ready for desktop

--- components/PostsCarousel.js
+++ components/PostsCarousel.js
@@ -1,20 +1,60 @@
 import Image from 'next/image';
-import TempImage from "@/assets/tmp/recent-img-tmp.png";
+import { useState } from 'react'
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
+import TempImage from "@/assets/tmp/recent-img-carousel.png";
+const tempTimeline = [
+    { image: TempImage, date: "January 20" },
+    { image: TempImage, date: "February 20" },
+    { image: TempImage, date: "March 20" },
+    { image: TempImage, date: "April 20" },
+    { image: TempImage, date: "June 20" },
+
+]
 
 export default function PostsCarousel() {
+    const [imageOffset, setImageOffset] = useState(0)
+
     return (
-        <div className="bg-beige">
-            <PostWidget image={TempImage} date="January 20" />
-            <PostWidget image={TempImage} date="February 20" />
-            <PostWidget image={TempImage} date="March 20" />
+        <div className="relative w-full h-full">
+            <div className="translate-y-24">
+                <div className="flex justify-center text-4xl md:text-6xl xl:text-8xl text-black"> 
+                    Recent Posts
+                </div>
+                <div className="flex justify-center pt-4 text-xl md:text-2xl text-black"> 
+                    Get involved with upcoming events 
+                </div>
+            </div>
+            <button  onClick={() => setImageOffset(imageOffset - 1)} className="absolute left-28 bottom-[40%] z-2 hover:scale-125 duration-200 cursor-pointer">
+                <AiOutlineArrowLeft  className="h-[40px] w-[40px]"/>
+            </button>
+            <div className="absolute right-28 bottom-[40%] hover:scale-125 duration-200 cursor-pointer">
+                <AiOutlineArrowRight onClick={() => setImageOffset(imageOffset + 1)} className="h-[40px] w-[40px]"/>
+            </div>
+            <div className="bg-beige flex h-fit md:mx-40 py-20 md:px-8 rounded-xl overflow-hidden">
+                <div 
+                style={{ transform: `translateX(-${33*imageOffset}%)`, transition: "all 1s ease" }}
+                className="flex flex-nowrap">
+                    {tempTimeline.map((post, idx)=> {
+                        return (
+                            <PostWidget image={post.image} date={post.date} />
+                        )
+                    })}
+                </div>
+                
+            </div>
         </div>
+        
     )
 }
 
 function PostWidget({ image, date }) {
     return (
-        <div>
-        </>
+        <div className="relative h-[400px] shrink-0 w-full md:w-1/3 md:p-6 text-center flex justify-center items-center">
+            <Image src={image} className="object-cover w-full h-full rounded-xl"/>
+            <div className="absolute w-full py-2.5 bottom-50 inset-x-0 text-white md:text-4xl lg:text-6xl">
+                {date}
+            </div>
+        </div>
     )
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -28,44 +28,43 @@ export default function EventsPage() {
                         Create <br /> Unforgettable <br /> Memories
                         </div>
                     </div>
-                    <div className="flex justify-center text-2xl md:text-4xl xl:text-5xl pt-12 pb-6 text-black"> 
-                        Recent Posts
-                    </div>
-                    <div className="flex justify-center pt-4 text-2xl md:text-4xl text-black"> 
-                        Get involved with upcoming events 
-                    </div>
+                  
+               
                     <PostsCarousel />
-                    <div className="flex justify-center text-2xl md:text-4xl xl:text-5xl pt-12 pb-6 text-black"> 
-                        Looking for more?
-                    </div>
-                    <div className="flex flex-wrap lg:flex-nowrap">
-                        <div className="w-full lg:w-[50%] text-black text-[25px] md:text-[35px] xl:text-[45px] text-center lg:text-left py-4 px-12">
-                            Check out our socials or linktr.ee/qasa for more events!
+                    <div className="-translate-y-20">
+                        <div className="flex justify-center text-2xl md:text-4xl xl:text-5xl pt-12 pb-6 text-black"> 
+                            Looking for more?
                         </div>
-                        <div className="w-full lg:w-[50%] flex justify-center flex-row flex-nowrap gap-2 py-6">
-                            {/** Facebook */}
-                            <Link href="https://www.facebook.com/queens.qasa"
-                                className="h-20 w-20 
-                                hover:shadow-xl duration-300 rounded-[22px]"
-                                target="_blank">
-                                    <Image src={FbLogo} />
-                            </Link>
-                                {/** Instagram */}
-                            <Link href="https://www.instagram.com/queens.asa/"
-                                className=" h-20 w-20 
-                                hover:shadow-xl duration-300 rounded-[22px]"
-                                target="_blank">
-                                    <Image src={InstaLogo} />
-                            </Link>
-                                {/** Linktree */}
-                            <Link href="https://linktr.ee/qasa"
-                                className="h-20 w-20 
-                                hover:shadow-xl duration-300 rounded-[22px]"
-                                target="_blank">
-                                    <Image src={TreeLogo} />
-                            </Link>
+                        <div className="flex flex-wrap lg:flex-nowrap">
+                            <div className="w-full lg:w-[50%] text-black text-[25px] md:text-[35px] xl:text-[45px] text-center lg:text-left py-4 px-12">
+                                Check out our socials or linktr.ee/qasa for more events!
+                            </div>
+                            <div className="w-full lg:w-[50%] flex justify-center flex-row flex-nowrap gap-2 py-6">
+                                {/** Facebook */}
+                                <Link href="https://www.facebook.com/queens.qasa"
+                                    className="h-20 w-20 
+                                    hover:shadow-xl duration-300 rounded-[22px]"
+                                    target="_blank">
+                                        <Image src={FbLogo} />
+                                </Link>
+                                    {/** Instagram */}
+                                <Link href="https://www.instagram.com/queens.asa/"
+                                    className=" h-20 w-20 
+                                    hover:shadow-xl duration-300 rounded-[22px]"
+                                    target="_blank">
+                                        <Image src={InstaLogo} />
+                                </Link>
+                                    {/** Linktree */}
+                                <Link href="https://linktr.ee/qasa"
+                                    className="h-20 w-20 
+                                    hover:shadow-xl duration-300 rounded-[22px]"
+                                    target="_blank">
+                                        <Image src={TreeLogo} />
+                                </Link>
+                            </div>
                         </div>
                     </div>
+                    
                 </div>
             </main>
         </>
